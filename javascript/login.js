ca3e451116b009ea5f884f6217cf2a32af071f35
joker99poker <96461807+joker99poker@users.noreply.github.com>
2022-10-18 17:32:17 -0400

    .

--- javascript/login.js
+++ javascript/login.js
@@ -1,7 +1,17 @@
+let email = document.getElementById("email").value
+let pass = document.getElementById("pass").value
+
 function check2() {
-    if (document.getElementById("pass").value === "") {
-        alert("Invaild Email or password");
-    } else {
+    if ( email === "" && pass === "") {
+        alert("Please type in the Email and password");
+    }
+    else {
         window.location.replace("YsammeMages.html");
     }
 }
+
+// similar behavior as an HTTP redirect
+//window.location.replace("http://stackoverflow.com");
+    
+// similar behavior as clicking on a link
+//window.location.href = "http://stackoverflow.com";
